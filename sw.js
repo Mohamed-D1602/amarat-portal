82f6eb8787fe3c29b9e57fff08239feb1de359b6
Mohamed-D1602 <mohamed1602@hotmail.com>
2026-08-03 09:23:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'amarat-v1';
+const CACHE = 'amarat-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
